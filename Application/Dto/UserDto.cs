550d1a8fa2d8696b272d28a796c4ce87701befe9
n.jaimakhanov <n.jaimakhanov@almau.edu.kz>
2025-10-14 03:01:09 +0500

    email nulleble

--- Application/Dto/UserDto.cs
+++ Application/Dto/UserDto.cs
@@ -4,6 +4,6 @@ public class UserDto
 {
     public string Login { get; set; }
     public string Password { get; set; }
-    public string Email { get; set; }
+    public string? Email { get; set; }
 
 }
